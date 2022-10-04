54e1db4fcb8e8df621f47bf7a20d91d066123c37
marck283 <86911602+marck283@users.noreply.github.com>
2022-10-04 23:08:24 +0200

    Risolto problema conversione id eventi pubblici da JSON a String

---
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/publicEvents/ViewHolder.java
@@ -0,0 +1,41 @@
+package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.publicEvents;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.R;
+import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.publicEvents.publicEvent.PublicEvent;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    private ImageView imgView;
+    private TextView evName, catName; //TextView per il nome dell'evento e la sua categoria
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imgView = itemView.findViewById(R.id.imageView2);
+        evName = itemView.findViewById(R.id.textView3);
+        catName = itemView.findViewById(R.id.textView5);
+
+        itemView.setOnClickListener(l -> {
+            //Avvia un'altra Activity che richiede le informazioni sull'evento
+            //Passare l'ID dell'evento tramite Bundle
+            Log.i("click", "card clicked");
+        });
+    }
+
+    public void bindData(PublicEvent dataModel, Context context) {
+        try {
+            imgView.setImageBitmap(dataModel.decodeBase64());
+            evName.setText(dataModel.getString("name"));
+            catName.setText(dataModel.getString("category"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
