d5a9bbeace74119bddeba1779e6dcf6286934e3c
marck283 <86911602+marck283@users.noreply.github.com>
2022-10-04 15:03:29 +0200

    Iniziato sviluppo schermata impostazioni

--- app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/ui/menu_settings/MenuSettingsViewModel.java
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/ui/menu_settings/MenuSettingsViewModel.java
@@ -1,8 +1,14 @@
 package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.ui.menu_settings;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.R;
 
 public class MenuSettingsViewModel extends ViewModel {
 
@@ -10,7 +16,13 @@ public class MenuSettingsViewModel extends ViewModel {
 
     public MenuSettingsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is menu_settings fragment");
+    }
+
+    public void showSettings(View root) {
+        TextView t1 = new TextView(root.getContext());
+        t1.setText("Mostra numero di telefono");
+        ConstraintLayout l = root.findViewById(R.id.constraintLayout);
+        l.addView(t1);
     }
 
     public LiveData<String> getText() {
