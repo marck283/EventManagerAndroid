06f7b10aae94425680aab00283ecb4e32b8b3c09
marck283 <86911602+marck283@users.noreply.github.com>
2022-11-14 15:12:28 +0100

    Aggiornato ViewModel

--- app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/ui/event_creation/newDate/NewDateFragment.java
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/ui/event_creation/newDate/NewDateFragment.java
@@ -2,6 +2,7 @@ package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.ui.event_
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jetbrains.annotations.Contract;
 
 import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.R;
 
@@ -18,6 +23,8 @@ public class NewDateFragment extends Fragment {
 
     private NewDateViewModel mViewModel;
 
+    @NonNull
+    @Contract(" -> new")
     public static NewDateFragment newInstance() {
         return new NewDateFragment();
     }
@@ -29,10 +36,30 @@ public class NewDateFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(NewDateViewModel.class);
-        // TODO: Use the ViewModel
+        Button b = requireActivity().findViewById(R.id.button3);
+        b.setOnClickListener(c -> {
+            EditText t = requireActivity().findViewById(R.id.begin_date);
+            EditText t1 = requireActivity().findViewById(R.id.begin_time);
+            EditText t2 = requireActivity().findViewById(R.id.venue_value);
+            EditText t3 = requireActivity().findViewById(R.id.seats_value);
+            if(t.getText() != null && t1.getText() != null && t2.getText() != null && t3.getText() != null) {
+                //Check strings for correct formats
+                try {
+                    int posti = Integer.parseInt(String.valueOf(t3.getText()));
+                } catch(NumberFormatException ex) {
+                    AlertDialog ad = new AlertDialog.Builder(requireContext()).create();
+                    ad.setTitle(R.string.incorrect_seats_format);
+                    ad.setMessage(getString(R.string.incorrect_seats_format));
+                    ad.setButton(AlertDialog.BUTTON_POSITIVE, "OK", (dialog1, which) -> dialog1.dismiss());
+                    ad.show();
+                }
+            } else {
+                //Tell user of missing or incorrect fields
+            }
+        });
     }
 
 }
--- app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/ui/event_creation/newDate/NewDateViewModel.java
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/ui/event_creation/newDate/NewDateViewModel.java
@@ -1,7 +1,49 @@
 package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.ui.event_creation.newDate;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class NewDateViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> data, ora, luogo;
+    private MutableLiveData<Integer> posti;
+
+    public NewDateViewModel() {
+        data = new MutableLiveData<>();
+        ora = new MutableLiveData<>();
+        luogo = new MutableLiveData<>();
+        posti = new MutableLiveData<>();
+    }
+
+    public void setData(String data) {
+        this.data.setValue(data);
+    }
+
+    public LiveData<String> getData() {
+        return data;
+    }
+
+    public void setOra(String ora) {
+        this.ora.setValue(ora);
+    }
+
+    public LiveData<String> getOra() {
+        return ora;
+    }
+
+    public void setLuogo(String indirizzo) {
+        luogo.setValue(indirizzo);
+    }
+
+    public LiveData<String> getLuogo() {
+        return luogo;
+    }
+
+    public void setPosti(int posti) {
+        this.posti.setValue(posti);
+    }
+
+    public LiveData<Integer> getPosti() {
+        return posti;
+    }
 }
