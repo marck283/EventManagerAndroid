a3dedcb294a0d525d6ce0cd9144292d4cbe0d24a
marck283 <86911602+marck283@users.noreply.github.com>
2023-02-01 04:43:24 +0100

    Aggiunta possibilità di avviare Google Maps tramite pressione sull'indirizzo dell'evento anche per gli utenti organizzatori

--- app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/eventInfo/organizedEvent/OrganizedEventInfo.java
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/eventInfo/organizedEvent/OrganizedEventInfo.java
@@ -1,5 +1,13 @@
 package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.eventInfo.organizedEvent;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +17,7 @@ import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
@@ -17,6 +26,7 @@ import com.journeyapps.barcodescanner.ScanOptions;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.R;
 import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.eventInfo.publicEvent.LuogoEvento;
@@ -52,6 +62,26 @@ public class OrganizedEventInfo extends Thread {
         this.launcher = launcher;
     }
 
+    private void startGoogleMaps(@NonNull EventDetailsFragment f, @NonNull TextView indirizzo,
+                                 @NonNull List<Address> addresses) {
+        Address address = addresses.get(0);
+
+        Uri gmURI = Uri.parse("geo:" + address.getLatitude() + "," + address.getLongitude()
+                + "?q=" + indirizzo.getText().toString());
+        Intent i = new Intent(Intent.ACTION_VIEW, gmURI);
+        i.setPackage("com.google.android.apps.maps");
+
+        f.requireActivity().startActivity(i);
+    }
+
+    private void noSuchAddressDialog(@NonNull Fragment f) {
+        AlertDialog dialog = new AlertDialog.Builder(f.requireActivity()).create();
+        dialog.setTitle(R.string.no_such_address);
+        dialog.setMessage(f.getString(R.string.address_not_registered));
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", (dialog1, which) -> dialog1.dismiss());
+        dialog.show();
+    }
+
     public void run() {
         Request request = new Request.Builder()
                 .addHeader("x-access-token", userJwt)
@@ -85,6 +115,39 @@ public class OrganizedEventInfo extends Thread {
 
                             TextView address = v.findViewById(R.id.textView15);
                             address.setText(f.getString(R.string.event_address, event.getLuogoEv().get(0).toString()));
+                            address.setOnClickListener(c -> {
+                                Geocoder geocoder = new Geocoder(f.requireActivity());
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                    geocoder.getFromLocationName(address.getText().toString(), 5, addresses -> {
+                                        if (addresses.size() > 0) {
+                                            startGoogleMaps(f, address, addresses);
+                                        } else {
+                                            noSuchAddressDialog(f);
+                                        }
+                                    });
+                                } else {
+                                    Thread t1 = new Thread() {
+                                        public void run() {
+                                            List<Address> addresses;
+                                            try {
+                                                addresses = geocoder.getFromLocationName(address.getText().toString(), 5);
+                                                if (addresses != null && addresses.size() > 0) {
+                                                    startGoogleMaps(f, address, addresses);
+                                                } else {
+                                                    Looper.prepare();
+                                                    noSuchAddressDialog(f);
+                                                    Looper.loop();
+                                                    Looper.getMainLooper().quitSafely();
+                                                }
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    };
+                                    t1.start();
+                                }
+                            });
+                            address.setPaintFlags(address.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
                             Spinner spinner = v.findViewById(R.id.spinner);
 
