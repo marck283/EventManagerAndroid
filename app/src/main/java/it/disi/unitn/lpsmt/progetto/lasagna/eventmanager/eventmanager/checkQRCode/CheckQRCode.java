d6ca215c4bac3375a3137a76d263205758f8c816
marck283 <86911602+marck283@users.noreply.github.com>
2023-05-05 20:23:15 +0200

    Aggiornate dipendenze; riscritto codice controllo codici QR biglietti

--- app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/checkQRCode/CheckQRCode.java
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/checkQRCode/CheckQRCode.java
@@ -1,24 +1,18 @@
 package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.checkQRCode;
 
-import android.app.AlertDialog;
 import android.util.Log;
 import android.util.Pair;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
-import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.R;
 import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.network.NetworkRequest;
 import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.network.networkOps.ServerOperation;
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.Request;
-import okhttp3.Response;
 
 public class CheckQRCode extends ServerOperation {
 
@@ -38,16 +32,6 @@ public class CheckQRCode extends ServerOperation {
         this.hour = hour;
     }
 
-    private void setAlertDialog(@StringRes int title, @StringRes int message) {
-        f.requireActivity().runOnUiThread(() -> {
-            AlertDialog dialog = new AlertDialog.Builder(f.requireActivity()).create();
-            dialog.setTitle(title);
-            dialog.setMessage(f.getString(message));
-            dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", (dialog1, which) -> dialog1.dismiss());
-            dialog.show();
-        });
-    }
-
     public void run() {
         List<Pair<String, String>> headers = new ArrayList<>();
         headers.add(new Pair<>("x-access-token", userJwt));
@@ -57,41 +41,10 @@ public class CheckQRCode extends ServerOperation {
         Request req = request.getRequest(headers,
                 getBaseUrl() + "/api/v2/QRCodeCheck/" + qrCode);
         Log.i("code", qrCode);
-        request.enqueue(req, new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                try {
-                    throw e;
-                } catch(IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) {
-                switch (response.code()) {
-                    case 200: {
-                        //OK
-                        setAlertDialog(R.string.valid_qr_code, R.string.valid_qr_code_message);
-                        break;
-                    }
-                    case 400: {
-                        //Richiesta malformata
-                        setAlertDialog(R.string.malformed_request, R.string.malformed_request_message);
-                        break;
-                    }
-                    case 401: {
-                        //Utente non autenticato
-                        setAlertDialog(R.string.user_not_logged_in, R.string.user_not_logged_in_message);
-                        break;
-                    }
-                    case 404: {
-                        //QR Code non valido
-                        setAlertDialog(R.string.qr_code_invalid, R.string.invalid_qr_code_message);
-                        break;
-                    }
-                }
-            }
-        });
+        try {
+            request.enqueue(req, new QRCodeCallback(f));
+        } catch (InvalidObjectException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/it/disi/unitn/lpsmt/progetto/lasagna/eventmanager/eventmanager/checkQRCode/QRCodeCallback.java
@@ -0,0 +1,74 @@
+package it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.checkQRCode;
+
+import android.app.AlertDialog;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InvalidObjectException;
+
+import it.disi.unitn.lpsmt.progetto.lasagna.eventmanager.eventmanager.R;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class QRCodeCallback implements Callback {
+
+    private final Fragment f;
+
+    public QRCodeCallback(@NotNull Fragment f) throws InvalidObjectException {
+        if(f == null) {
+            throw new InvalidObjectException("The argument to this constructor cannot be null.");
+        }
+        this.f = f;
+    }
+
+    private void setAlertDialog(@StringRes int title, @StringRes int message) {
+        f.requireActivity().runOnUiThread(() -> {
+            AlertDialog dialog = new AlertDialog.Builder(f.requireActivity()).create();
+            dialog.setTitle(title);
+            dialog.setMessage(f.getString(message));
+            dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", (dialog1, which) -> dialog1.dismiss());
+            dialog.show();
+        });
+    }
+
+    @Override
+    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+        try {
+            throw e;
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onResponse(@NonNull Call call, @NonNull Response response) {
+        switch (response.code()) {
+            case 200: {
+                //OK
+                setAlertDialog(R.string.valid_qr_code, R.string.valid_qr_code_message);
+                break;
+            }
+            case 400: {
+                //Richiesta malformata
+                setAlertDialog(R.string.malformed_request, R.string.malformed_request_message);
+                break;
+            }
+            case 401: {
+                //Utente non autenticato
+                setAlertDialog(R.string.user_not_logged_in, R.string.user_not_logged_in_message);
+                break;
+            }
+            case 404: {
+                //QR Code non valido
+                setAlertDialog(R.string.qr_code_invalid, R.string.invalid_qr_code_message);
+                break;
+            }
+        }
+    }
+}
